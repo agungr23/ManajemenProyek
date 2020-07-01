f95c2a69f3071f24e084c23a096eab84ecfe1cd2
agungr23 <agungr439@gmail.com>
2020-07-01 16:30:51 +0700

    Manajemen Proyek

--- application/views/admin/forgot_password.php
+++ application/views/admin/forgot_password.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="<?php echo base_url('assets/img/kodegiri_putih.png')?>" rel="shortcut icon">
     <title>Forgot Password</title>
     <style type="text/css">
         .colorbd{
